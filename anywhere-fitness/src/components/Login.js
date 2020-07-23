fd34dba4c85d2cd70e7a194445753681cd92a708
Aric Repp <aricmrepp@gmail.com>
2020-07-22 23:15:17 -0400

    Login/Reg placeholders

--- anywhere-fitness/src/components/Login.js
+++ anywhere-fitness/src/components/Login.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
+import { Image, Container, Header } from 'semantic-ui-react';
 import logo from '../assets/logo_size.jpg';
+import './Login.css';
 
 const Login = () => {
   return (
-    <div>
-      <header>
+    <div className="page_root">
+      <header className="header">
         <Image src={logo}></Image>
       </header>
+      <Container className="login_root">
+        <div className="login_container">
+          <Header as="h2">Login</Header>
+        </div>
+      </Container>
     </div>
   );
 };
