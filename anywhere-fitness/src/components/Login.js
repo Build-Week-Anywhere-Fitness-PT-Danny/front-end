1d3dcac3151707398be6a9c32b02375e1304c661
Aric Repp <aricmrepp@gmail.com>
2020-07-22 21:35:35 -0400

    Pushing CRA, Dashboard, connection setup

---
+++ anywhere-fitness/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+import logo from '../assets/logo_size.jpg';
+
+const Login = () => {
+  return (
+    <div>
+      <header>
+        <Image src={logo}></Image>
+      </header>
+    </div>
+  );
+};
+
+export default Login;
