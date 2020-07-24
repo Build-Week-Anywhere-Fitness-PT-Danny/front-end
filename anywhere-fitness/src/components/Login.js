4e1508bddb52e1e38db0c79b85663cdbd1b6cdfc
Aric Repp <aricmrepp@gmail.com>
2020-07-23 23:20:54 -0400

    Login to DshBrd, DshBrd design, working on search

--- anywhere-fitness/src/components/Login.js
+++ anywhere-fitness/src/components/Login.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, Container, Header } from 'semantic-ui-react';
+import axios from 'axios';
 import logo from '../assets/logo_size.jpg';
 import './Login.css';
 
-const Login = () => {
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        'https://anywhere-fitness-bw-2020.herokuapp.com/api/auth/login',
+        credentials
+      )
+      .then((res) => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/protected');
+      })
+      .catch((err) => console.log({ err }));
+  };
+
   return (
     <div className="page_root">
       <header className="header">
@@ -12,6 +36,25 @@ const Login = () => {
       <Container className="login_root">
         <div className="login_container">
           <Header as="h2">Login</Header>
+          <div className="input_con">
+            <form className="login_form" onSubmit={login}>
+              <input
+                className="login_input"
+                type="text"
+                name="username"
+                value={credentials.username}
+                onChange={handleChange}
+              />
+              <input
+                className="login_input"
+                type="password"
+                name="password"
+                value={credentials.password}
+                onChange={handleChange}
+              />
+              <button>Log in</button>
+            </form>
+          </div>
         </div>
       </Container>
     </div>
