458c3d57a4c981c4a19f5d296dcc51ebf49907a8
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-07-27 18:27:34 -0500

    added my components

--- anywhere-fitness/src/Components/Instructor/SavedWorkoutList.js
+++ anywhere-fitness/src/Components/Instructor/SavedWorkoutList.js
@@ -1,31 +1,21 @@
-import React from 'react'
-import SwipeToDelete from 'react-swipe-to-delete-component';
+import React,{ useState } from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
+import CreateWorkout from './CreateWorkout'
+import SavedWorkoutList from './SavedWorkoutList'
 
-
-function SavedWorkoutList(props) {
-
+function InstructorApp() {
+    const [savedClass, setSavedClass] = useState([]);
     return (
-        <div>
-            
-        <div className='workoutCard'>
-            {props.savedClass.map((data) => (
-                <div className="workoutCard">
-                    <h2>{data.createClass.name}</h2>
-                    <h3>{data.createClass.type}</h3>
-                    <h3>{data.createClass.startTime}</h3>
-                    <h3>{data.createClass.duration}</h3>
-                    <h3>{data.createClass.intensity}</h3>
-                    <h3>{data.createClass.location}</h3>
-                    <h3>{data.createClass.numberOfRegisteredAttendees}</h3>
-                    <h3>{data.createClass.maxClassSize}</h3>
-                </div>
-            ))}
-
-        </div>
-    </div>
+        <Router>
+             <div>
+                <Link to="/CreateWorkout">Create A Workout Class</Link>
+                <Route to="/CreateWorkout"><CreateWorkout savedClass={savedClass} setSavedClass={setSavedClass} /></Route>
+                <Link to="/SavedWorkoutList">Saved Workouts</Link>
+                <Route path="/SavedWorkoutList"><SavedWorkoutList savedClass={savedClass} /></Route>
+            </div>
+        </Router>
     )
 }
 
-export default SavedWorkoutList
-
-//Make Workouts Removable
+export default InstructorApp
