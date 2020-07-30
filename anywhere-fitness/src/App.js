dc454de4d4e000a0f9970cb26c0b16136e8188a4
Aric Repp <aricmrepp@gmail.com>
2020-07-29 22:58:22 -0400

    new branch

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './components/Dashboard';
 import CreateClass from './components/CreateClass';
-import Login from './components/Login-Aric';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
