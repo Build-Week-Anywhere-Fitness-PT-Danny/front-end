910c1073e2ed1f7d61e5760e5b29e80dafb6dfbf
Aric Repp <aricmrepp@gmail.com>
2020-07-30 21:15:19 -0400

    push

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './components/Dashboard';
 import CreateClass from './components/CreateClass';
 import Login from './components/Login';
-import * as SignUp from './components/Components/Register/SignUp';
+// import SignUp from './components/Components/Register/SignUp';
+import Register from './components/Register';
 import './App.css';
 
 function App() {
@@ -16,7 +18,7 @@ function App() {
         </Route> */}
         <Switch>
           <Route path="/login" component={Login} />
-          <Route path="/register" component={SignUp} />
+          <Route path="/register" component={Register} />
           <PrivateRoute path="/protected" component={Dashboard} />
           <PrivateRoute path="/protected/create" component={CreateClass} />
         </Switch>
--- anywhere-fitness/src/components/Register.js
+++ anywhere-fitness/src/components/Register.js
@@ -1,19 +1,148 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image, Container, Header } from 'semantic-ui-react';
 import logo from '../assets/logo_size.jpg';
 import './Login.css';
+import axios from 'axios';
+import * as Yup from 'yup';
 
 const Register = () => {
+  const [values, setValues] = useState({
+    fullname: '',
+    username: '',
+    password: '',
+    admin: false,
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  const handleChange = (event) => {
+    const usertype =
+      event.target.type === 'checkbox'
+        ? event.target.checked
+        : event.target.value;
+    setValues({
+      ...values,
+      [event.target.name]: usertype,
+    });
+    validateChange(event);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('form submitted!');
+    axios
+      .post('https://reqres.in/api/users', values)
+      .then(() => console.log('form submitted success'));
+    console.log(values);
+  };
+
+  const [errors, setErrors] = useState({
+    fullname: '',
+    username: '',
+    password: '',
+    admin: false,
+  });
+
+  const formSchema = Yup.object().shape({
+    fullname: Yup.string()
+      .min(5, 'Must include Full Name')
+      .required('Must include Full Name'),
+    username: Yup.string()
+      .min(5, 'username must be atleast 5 characters long')
+      .required('username is required'),
+    password: Yup.string()
+      .min(5, 'Password must be 5-10 characters long')
+      .required(),
+    admin: Yup.boolean(),
+  });
+
+  useEffect(() => {
+    formSchema.isValid(values).then((valid) => {
+      setButtonDisabled(!valid);
+    });
+  }, [values]);
+
+  const validateChange = (e) => {
+    e.persist();
+    Yup.reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then(() =>
+        setErrors({
+          ...errors,
+          [e.target.name]: '',
+        })
+      )
+      .catch((error) =>
+        setErrors({
+          ...errors,
+          [e.target.name]: error.errors[0],
+        })
+      );
+  };
+
   return (
-    <div className="page_root">
-      <header className="header">
-        <Image src={logo}></Image>
-      </header>
-      <Container className="login_root">
-        <div className="login_container">
-          <Header as="h2">Register</Header>
-        </div>
-      </Container>
+    <div>
+      <form onSubmit={handleSubmit} className="formDiv">
+        <h1>Register</h1>
+        <label htmlFor="fullname">
+          {' '}
+          First and Last Name:
+          <input
+            type="text"
+            name="fullname"
+            value={values.fullname}
+            onChange={handleChange}
+            error={errors.fullname}
+            style={{ width: '300px' }}
+          />
+          {errors.fullname.length > 0 ? <p>{errors.fullname}</p> : null}
+        </label>
+
+        <label htmlFor="username">
+          {' '}
+          Username :
+          <input
+            type="text"
+            name="username"
+            value={values.username}
+            onChange={handleChange}
+            error={errors.username}
+            style={{ width: '300px' }}
+          />
+          {errors.username.length > 0 ? <p>{errors.username}</p> : null}
+        </label>
+
+        <label htmlFor="password">
+          {' '}
+          Password :
+          <input
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            error={errors.password}
+            style={{ width: '300px' }}
+          />
+          {errors.password.length > 0 ? <p>{errors.password}</p> : null}
+        </label>
+        <label htmlFor="admin">
+          Instructor:
+          <input
+            type="checkbox"
+            name="admin"
+            value={values.admin}
+            onChange={handleChange}
+          />
+        </label>
+
+        <input
+          disabled={buttonDisabled}
+          id="submit"
+          type="submit"
+          name="submit"
+          className="submitBtn"
+        />
+      </form>
     </div>
   );
 };
